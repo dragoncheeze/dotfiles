11a092ab568d8d8ccbe842e1c7c09955659ff54a
dragoncheeze <colby.braden@gmail.com>
2021-03-28 00:57:25 -0500

    added default script

--- debian-install-scripts/android.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-#########################
-#     Android
-#########################
-
-sudo apt install adb fastboot android-sdk-libsparse-utils android-sdk-platform23 -yy
-
---
+++ debian-install-scripts/defaults.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+sudo apt install fish xserver-xorg xorg picom lxappearance rofi libxft-dev libx11-dev libxinerama-dev geany qutebrowser suckless-tools build-essential curl wget cargo sxiv
