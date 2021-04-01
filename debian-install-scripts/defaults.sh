4ae673561434829093bdaa8fd763ff048947d04c
dragoncheeze <colby.braden@gmail.com>
2021-03-31 20:18:15 -0500

    added full.sh script for installs

--- debian-install-scripts/defaults.sh
+++ debian-install-scripts/defaults.sh
@@ -1,2 +1,4 @@
 #!/bin/sh
+# Default programs
 sudo apt install fish xserver-xorg xorg fonts-ubuntu picom lxappearance pcmanfm xarchiver rofi libxft-dev libx11-dev libxinerama-dev geany qutebrowser suckless-tools build-essential curl wget cargo sxiv maim nitrogen alsa-utils volumeicon-alsa neofetch htop
+
