86c903856c47e43dae21c299a244c5749f446afd
dragoncheeze <colby.braden@gmail.com>
2021-04-16 01:33:03 -0500

    updated various scripts and configs

--- debian-install-scripts/defaults.sh
+++ debian-install-scripts/defaults.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 # Default programs
-sudo apt install fish xserver-xorg xorg fonts-ubuntu picom lxappearance pcmanfm xarchiver rofi libxft-dev libx11-dev libxinerama-dev geany qutebrowser suckless-tools build-essential curl wget cargo sxiv maim nitrogen alsa-utils pasystray neofetch htop polybar bspwm -yy
+sudo apt install fish xserver-xorg xorg fonts-ubuntu picom lxappearance pcmanfm xarchiver rofi libxft-dev libx11-dev libxinerama-dev geany qutebrowser suckless-tools build-essential curl wget cargo sxiv maim nitrogen alsa-utils pasystray neofetch htop powerline polybar bspwm -yy
 
