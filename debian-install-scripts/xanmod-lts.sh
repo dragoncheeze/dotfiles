45beae0fd93d158503a19d326148b78ce5d0fdab
dragoncheeze <colby.braden@gmail.com>
2021-04-16 01:20:25 -0500

    updated various scripts and configs

---
+++ debian-install-scripts/xanmod-lts.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+# Add repository
+echo 'deb http://deb.xanmod.org releases main' | sudo tee /etc/apt/sources.list.d/xanmod-kernel.list
+
+# GPG key
+wget -qO - https://dl.xanmod.org/gpg.key | sudo apt-key --keyring /etc/apt/trusted.gpg.d/xanmod-kernel.gpg add -
+
+# Install kernel
+sudo apt update && sudo apt install linux-xanmod-lts preload gamemode
