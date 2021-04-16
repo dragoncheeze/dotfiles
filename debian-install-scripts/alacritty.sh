b54facf2c2cdf5ca4a7f91c7538993c5206193c6
dragoncheeze <colby.braden@gmail.com>
2021-04-16 01:51:48 -0500

    updated alacritty install

--- debian-install-scripts/alacritty.sh
+++ debian-install-scripts/alacritty.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+sudo apt-get install cmake pkg-config libfreetype6-dev libfontconfig1-dev libxcb-xfixes0-dev python3 -yy &&
 cd ~/builds/
 git clone https://github.com/alacritty/alacritty.git
 cd alacritty/
