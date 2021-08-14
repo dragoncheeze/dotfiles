738d2be51a87a3d55bb1cd74a0b7901db56cfca2
dragoncheeze <75183285+dragoncheeze@users.noreply.github.com>
2021-08-14 09:03:29 -0500

    Update liquorix.sh

--- debian-install-scripts/liquorix.sh
+++ debian-install-scripts/liquorix.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 curl 'https://liquorix.net/add-liquorix-repo.sh' | sudo bash &&
-sudo apt-get install linux-image-liquorix-amd64 linux-headers-liquorix-amd64
+sudo apt install linux-image-liquorix-amd64 linux-headers-liquorix-amd64
