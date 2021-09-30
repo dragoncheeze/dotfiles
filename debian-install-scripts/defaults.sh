8e1e904c93baf3e7080b5e423c352e65b5c5751f
dragoncheeze <75183285+dragoncheeze@users.noreply.github.com>
2021-09-30 05:39:51 -0500

    Update defaults.sh

--- debian-install-scripts/defaults.sh
+++ debian-install-scripts/defaults.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 # Default programs
-sudo apt install fish install apt-listbugs apt-listchanges xserver-xorg xorg fonts-ubuntu libxkbcommon-dev picom lxappearance conky pavucontrol pulsemixer papirus-icon-theme pcmanfm xarchiver rofi libxft-dev libx11-dev libxinerama-dev galculator geany qutebrowser suckless-tools build-essential curl wget cargo sxiv scrot nitrogen alsa-utils pasystray neofetch htop powerline gamemode polybar bspwm -yy
+sudo apt install fish apt-listbugs apt-listchanges xserver-xorg xorg fonts-ubuntu libxkbcommon-dev picom lxappearance conky pavucontrol pulsemixer papirus-icon-theme pcmanfm xarchiver rofi libxft-dev libx11-dev libxinerama-dev galculator geany qutebrowser suckless-tools build-essential curl wget cargo sxiv scrot nitrogen alsa-utils pasystray neofetch htop powerline gamemode polybar bspwm -yy
 
