80179fd58b4cc51c8d301a53cfb3002c91276c91
dragoncheeze <75183285+dragoncheeze@users.noreply.github.com>
2021-10-21 00:59:57 +0000

    Add files via upload

---
+++ defaults-arch.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+# Default programs
+yay -S xorg-xinit xorg-apps gruvbox-material-icon-theme-git gruvbox-material-gtk-theme-git libxkbcommon-dev picom lxappearance conky pavucontrol pulsemixer papirus-icon-theme pcmanfm xarchiver rofi galculator geany qutebrowser lxsession curl wget cargo sxiv scrot nitrogen alsa-utils neofetch htop powerline mpv gamemode polybar bspwm sxhkd -yy
+
