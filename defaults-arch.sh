f17e80ef3dc04e9e817b34130ec4bb51ec988ed8
dragoncheeze <75183285+dragoncheeze@users.noreply.github.com>
2021-10-21 08:09:23 -0500

    Update defaults-arch.sh

--- defaults-arch.sh
+++ defaults-arch.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 # Default programs
-yay -S linux-zen-headers xorg-xinit xorg-apps gruvbox-material-icon-theme-git gruvbox-material-gtk-theme-git libxkbcommon-dev picom lxappearance conky pavucontrol pulsemixer papirus-icon-theme pcmanfm xarchiver rofi galculator geany qutebrowser lxsession curl wget cargo sxiv scrot nitrogen alsa-utils neofetch htop powerline mpv gamemode polybar bspwm sxhkd -yy
+yay -S linux-zen-headers xorg-xinit xorg-apps gruvbox-material-icon-theme-git gruvbox-material-gtk-theme-git lib32-gnutls lib32-libldap lib32-libgpg-error lib32-sqlite lib32-libpulse lib32-alsa-plugins libxkbcommon-dev picom lxappearance conky pavucontrol pulsemixer papirus-icon-theme pcmanfm xarchiver rofi galculator geany qutebrowser lxsession curl wget cargo sxiv scrot nitrogen alsa-utils neofetch htop powerline mpv gamemode polybar bspwm sxhkd -yy
 
