77f5fe8844c8982714154355ddff0dc07a171bd2
dragoncheeze <75183285+dragoncheeze@users.noreply.github.com>
2021-10-26 01:19:25 +0000

    Update defaults-arch.sh

--- arch/defaults-arch.sh
+++ arch/defaults-arch.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 # Default programs
-yay -S linux-zen-headers man xorg-xinit cups ipp-usb xorg-apps python-psutil fuse wine-staging lutris alacritty brave youtube-dl dmenu gruvbox-material-icon-theme-git gruvbox-material-gtk-theme-git ttf-ubuntu-font-family lib32-gnutls lib32-libldap lib32-libgpg-error lib32-sqlite lib32-libpulse lib32-alsa-plugins picom lxappearance conky pavucontrol pulsemixer papirus-icon-theme pcmanfm xarchiver rofi galculator geany qutebrowser lxsession curl wget cargo sxiv scrot nitrogen alsa-utils neofetch htop powerline mpv gamemode polybar bspwm sxhkd -yy
+yay -S linux-zen-headers man xorg-xinit cups ipp-usb xorg-apps python-psutil fuse dunst wine-staging lutris alacritty brave youtube-dl dmenu gruvbox-material-icon-theme-git gruvbox-material-gtk-theme-git ttf-ubuntu-font-family lib32-gnutls lib32-libldap lib32-libgpg-error lib32-sqlite lib32-libpulse lib32-alsa-plugins picom lxappearance conky pavucontrol pulsemixer papirus-icon-theme pcmanfm xarchiver rofi galculator geany qutebrowser lxsession curl wget cargo sxiv scrot nitrogen alsa-utils neofetch htop powerline mpv gamemode polybar bspwm sxhkd -yy
 
