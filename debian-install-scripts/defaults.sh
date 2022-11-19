ee39d3e1699200dd396fc3fb68b301fe23fcca69
dragoncheeze <75183285+dragoncheeze@users.noreply.github.com>
2022-11-19 10:23:47 -0600

    Update defaults.sh

--- debian-install-scripts/defaults.sh
+++ debian-install-scripts/defaults.sh
@@ -1,4 +1,3 @@
 #!/bin/sh
 # Default programs
-sudo apt install fish apt-listbugs apt-listchanges xserver-xorg xorg rsync gtk2-engines-murrine fonts-ubuntu libxkbcommon-dev picom lxappearance conky pavucontrol pulsemixer papirus-icon-theme pcmanfm xarchiver rofi libxft-dev libx11-dev libxinerama-dev galculator geany suckless-tools build-essential curl wget cargo sxiv scrot nitrogen alsa-utils neofetch htop powerline mpv arc-theme synaptic gamemode polybar bspwm cups -yy
-
+sudo apt install apt-listbugs apt-listchanges xserver-xorg xorg lxpolkit rsync gtk2-engines-murrine fonts-ubuntu libxkbcommon-dev picom lxappearance conky pavucontrol pulsemixer materia-gtk-theme papirus-icon-theme pcmanfm xarchiver rofi libxft-dev libx11-dev libxinerama-dev galculator geany dunst libnotify-bin suckless-tools build-essential curl wget sxiv scrot nitrogen neofetch htop mpv arc-theme synaptic gamemode polybar bspwm cups podman crun -yy
