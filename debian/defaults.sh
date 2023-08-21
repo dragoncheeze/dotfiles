4ac85a49983930e6e022de878bc792bd3572b690
Colby <colby.braden@gmail.com>
2023-08-21 10:29:19 -0500

    updated debian config

--- debian/defaults.sh
+++ debian/defaults.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 ## Default programs
-sudo apt install xserver-xorg xorg lxpolkit rsync gtk2-engines-murrine fonts-ubuntu fonts-dejavu fonts-font-awesome fonts-liberation2 fonts-noto fonts-roboto libxkbcommon-dev picom lxappearance conky pavucontrol pulsemixer materia-gtk-theme arc-theme gnome-themes-extra papirus-icon-theme pcmanfm xarchiver rofi libxft-dev libx11-dev libxinerama-dev galculator geany dunst libnotify-bin suckless-tools build-essential curl wget sxiv scrot nitrogen neofetch htop mpv arc-theme synaptic gamemode polybar bspwm i3 i3status playerctl cups pipewire light -y &&
+sudo apt install xserver-xorg xorg lxpolkit rsync gtk2-engines-murrine fonts-ubuntu fonts-dejavu fonts-font-awesome fonts-liberation2 fonts-noto fonts-roboto libxkbcommon-dev picom lxappearance conky pavucontrol pulsemixer materia-gtk-theme arc-theme gnome-themes-extra papirus-icon-theme pcmanfm xarchiver rofi libxft-dev libx11-dev libxinerama-dev galculator geany dunst libnotify-bin suckless-tools build-essential curl wget sxiv scrot feh neofetch htop mpv arc-theme synaptic gamemode polybar bspwm i3 i3status playerctl cups pipewire light -y &&
 
 ## Gaming
 sudo dpkg --add-architecture i386 &&
