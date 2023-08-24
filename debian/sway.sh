32a2636e2381cc0e2f9cdbb35805b3e75796b23e
Colby Braden <colby.braden@gmail.com>
2023-08-23 23:12:58 -0500

    updated sway again

--- debian/sway.sh
+++ debian/sway.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 ## Install Sway Stuff
-sudo apt install sway swaybg swaylock swayidle python3-i3ipc seatd light playerctl pulseaudio pipewire pipewire-pulse wireplumber pavucontrol pulsemixer wl-clipboard neofetch gtk2-engines-murrine mpv mpd foot waybar papirus-icon-theme arc-theme wofi fonts-dejavu fonts-font-awesome fonts-liberation2 fonts-noto fonts-roboto lxpolkit libglib2.0-bin xwayland thunar thunar-archive-plugin file-roller thunar-media-tags-plugin thunar-volman tumbler cups galculator build-essential gamemode imv -y &&
+sudo apt install sway swaybg swaylock swayidle python3-i3ipc seatd dunst light playerctl pulseaudio pipewire pipewire-pulse wireplumber pavucontrol pulsemixer wl-clipboard neofetch gtk2-engines-murrine mpv mpd foot waybar papirus-icon-theme arc-theme wofi fonts-dejavu fonts-font-awesome fonts-liberation2 fonts-noto fonts-roboto lxpolkit libglib2.0-bin xwayland thunar thunar-archive-plugin file-roller thunar-media-tags-plugin thunar-volman tumbler cups galculator build-essential gamemode imv -y &&
 ## Nvidia
 # sudo apt install nvidia-driver -y &&
 ## Flatpak Stuff
