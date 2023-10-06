45ad07a8c70a64c51f33afb1f899f38ea4330ad8
Colby <colby.braden@gmail.com>
2023-10-06 07:56:23 -0500

    updated configs

--- debian/flatpak.sh
+++ debian/flatpak.sh
@@ -1,7 +1,3 @@
 #!/bin/bash
 sudo apt install flatpak -y &&
-flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo &&
-flatpak --user override --filesystem=/home/$USER/.icons/:ro &&
-flatpak --user override --filesystem=/usr/share/icons/:ro &&
-sudo flatpak override --filesystem=/usr/share/themes/ &&
-sudo flatpak override --env GTK_THEME=Adwaita-dark
+flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
--- debian/sway.sh
+++ debian/sway.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 ## Install Sway Stuff
-sudo apt install sway swaybg swaylock swayidle python3-i3ipc seatd dunst light playerctl pipewire pipewire-pulse wireplumber pavucontrol pulsemixer wl-clipboard neofetch gtk2-engines-murrine mpv mpd foot waybar papirus-icon-theme arc-theme wofi fonts-dejavu fonts-font-awesome fonts-liberation2 fonts-noto fonts-roboto lxpolkit libglib2.0-bin xwayland thunar thunar-archive-plugin file-roller thunar-media-tags-plugin thunar-volman tumbler cups galculator build-essential gamemode imv -y &&
+sudo apt install sway swaybg swaylock swayidle python3-i3ipc seatd dunst light playerctl pipewire pipewire-pulse wireplumber pavucontrol pulsemixer wl-clipboard neofetch gtk2-engines-murrine mpv mpd foot waybar papirus-icon-theme arc-theme wofi fonts-dejavu fonts-font-awesome fonts-liberation2 fonts-noto fonts-roboto lxpolkit libglib2.0-bin xwayland thunar thunar-archive-plugin file-roller thunar-media-tags-plugin thunar-volman tumbler cups galculator build-essential gamemode imv xdg-desktop-portal-gtk -y &&
 ## Nvidia
 # sudo apt install nvidia-driver -y &&
