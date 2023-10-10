31cd7f4ec2548e754b8d74a2ed77d80d1204d35f
dragoncheeze <75183285+dragoncheeze@users.noreply.github.com>
2023-10-10 08:40:00 -0500

    Update sway.sh

--- arch/sway.sh
+++ arch/sway.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-sudo pacman -S --needed sway swaylock swayidle swaybg foot waybar wofi seatd light playerctl arc-gtk-theme papirus-icon-theme firefox lxappearance pipewire pipewire-pulse wireplumber pavucontrol pulsemixer wl-clipboard man-db neofetch gtk-engine-murrine mpd mpv xorg-xwayland thunar thunar-archive-plugin thunar-media-tags-plugin gvfs thunar-volman tumbler file-roller lxsession gamemode flatpak polkit cups ipp-usb ghostscript dunst imv libadwaita &&
+sudo pacman -S --needed sway swaylock swayidle swaybg foot waybar wofi seatd light playerctl arc-gtk-theme papirus-icon-theme firefox lxappearance pipewire pipewire-pulse wireplumber wl-clipboard man-db neofetch gtk-engine-murrine mpd mpv xorg-xwayland thunar thunar-archive-plugin thunar-media-tags-plugin gvfs thunar-volman tumbler file-roller lxsession gamemode flatpak polkit cups ipp-usb ghostscript dunst imv libadwaita &&
 # Fonts
 sudo pacman -S --needed otf-font-awesome ttf-croscore ttf-dejavu ttf-droid gnu-free-fonts ttf-liberation noto-fonts ttf-roboto ttf-ubuntu-font-family &&
 # Trim
