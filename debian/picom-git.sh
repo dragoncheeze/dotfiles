0e8aa8ad603c97dac07ba94a86b319bfc3bc3b54
Colby <colby.braden@gmail.com>
2023-12-17 08:07:18 -0600

    updated picom-git script

--- debian/picom-git.sh
+++ debian/picom-git.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 sudo apt install libconfig-dev libdbus-1-dev libegl-dev libev-dev libgl-dev libpcre2-dev libpixman-1-dev libx11-xcb-dev libxcb1-dev libxcb-composite0-dev libxcb-damage0-dev libxcb-dpms0-dev libxcb-glx0-dev libxcb-image0-dev libxcb-present-dev libxcb-randr0-dev libxcb-render0-dev libxcb-render-util0-dev libxcb-shape0-dev libxcb-util-dev libxcb-xfixes0-dev libxext-dev meson ninja-build uthash-dev -y &&
-git clone https://github.com/yshui/picom.git &&
-cd picom &&
+git clone https://github.com/yshui/picom.git  ~/builds/picom/ &&
+cd ~/builds/picom &&
 meson setup --buildtype=release build &&
 ninja -C build &&
 sudo ninja -C build install
