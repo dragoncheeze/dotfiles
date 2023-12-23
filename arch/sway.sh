4ed604baae028f1615a98f17420adf28c2a0ef4b
Colby <colby.braden@gmail.com>
2023-12-22 23:26:34 -0600

    updated sway install script

--- arch/sway.sh
+++ arch/sway.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
-sudo pacman -S --needed sway swaylock swayidle swaybg i3status foot rofi seatd light playerctl arc-gtk-theme papirus-icon-theme firefox lxappearance pipewire pipewire-pulse wireplumber wl-clipboard man-db neofetch gtk-engine-murrine mpd mpv xorg-xwayland thunar thunar-archive-plugin thunar-media-tags-plugin gvfs thunar-volman tumbler file-roller lxsession gamemode flatpak polkit cups ipp-usb ghostscript dunst imv libadwaita gnome-themes-extra &&
-# Fonts
-sudo pacman -S --needed otf-font-awesome ttf-croscore ttf-dejavu ttf-droid gnu-free-fonts ttf-liberation noto-fonts ttf-roboto ttf-ubuntu-font-family &&
+yay -S --needed sway swaylock swayidle swaybg brave-bin autotiling i3status \
+    foot rofi seatd light playerctl arc-gtk-theme papirus-icon-theme firefox \
+    lxappearance pipewire pipewire-pulse wireplumber wl-clipboard man-db neofetch \
+    gtk-engine-murrine mpd mpv xorg-xwayland pcmanfm gvfs file-roller lxsession \
+    gamemode flatpak polkit cups ipp-usb ghostscript dunst imv libadwaita \
+    gnome-themes-extra otf-font-awesome ttf-croscore ttf-dejavu ttf-droid \
+    gnu-free-fonts ttf-liberation noto-fonts ttf-roboto ttf-ubuntu-font-family &&
 # Cups
 sudo systemctl enable cups &&
-sudo systemctl start cups &&
-# Theme
-gsettings set org.gnome.desktop.interface icon-theme 'Papirus-Dark' &&
-gsettings set org.gnome.desktop.interface gtk-theme 'Arc-Dark'
+sudo systemctl start cups
