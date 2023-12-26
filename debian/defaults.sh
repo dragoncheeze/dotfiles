62f7dc70b7cdf44f3214a44e857cdc8caff522da
Colby <colby.braden@gmail.com>
2023-12-26 09:36:35 -0600

    updated i3 configs and default install script

--- debian/defaults.sh
+++ debian/defaults.sh
@@ -6,6 +6,6 @@ sudo apt install xserver-xorg xorg lxpolkit rsync gtk2-engines-murrine \
     pavucontrol pulsemixer materia-gtk-theme arc-theme gnome-themes-extra \
     papirus-icon-theme pcmanfm xarchiver rofi libxft-dev libx11-dev libxinerama-dev \
     galculator geany dunst libnotify-bin suckless-tools build-essential curl \
-    wget sxiv scrot feh neofetch htop mpv arc-theme synaptic gamemode \
+    wget sxiv scrot feh neofetch htop mpv arc-theme synaptic gamemode wine \
     polybar bspwm i3 i3status playerctl cups pipewire -y
 
