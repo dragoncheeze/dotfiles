ccfbf9775053edb076eaa1d7ed200d1ef39c9c6a
Colby <colby.braden@gmail.com>
2024-01-10 09:17:49 -0600

    updated sway install

--- arch/sway.sh
+++ arch/sway.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 yay -S --needed sway swaylock swayidle swaybg brave-bin autotiling i3status \
-    foot rofi seatd light playerctl arc-gtk-theme papirus-icon-theme firefox \
+    foot rofi seatd playerctl arc-gtk-theme papirus-icon-theme \
     lxappearance pipewire pipewire-pulse wireplumber wl-clipboard man-db neofetch \
-    gtk-engine-murrine mpd mpv xorg-xwayland pcmanfm gvfs file-roller lxsession \
+    gtk-engine-murrine mpd mpv xorg-xwayland gvfs file-roller lxsession \
     gamemode flatpak polkit cups ipp-usb ghostscript dunst imv libadwaita \
     gnome-themes-extra otf-font-awesome ttf-croscore ttf-dejavu ttf-droid \
     gnu-free-fonts ttf-liberation noto-fonts ttf-roboto ttf-ubuntu-font-family &&
