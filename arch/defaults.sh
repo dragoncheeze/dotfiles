baa2a7e51c3efa80049d7540256efb7b81b20e21
Colby <colby.braden@gmail.com>
2024-01-09 20:12:07 -0600

    updated default script

--- arch/defaults.sh
+++ arch/defaults.sh
@@ -4,7 +4,7 @@ yay -S rsync man pacman-contrib reflector xclip xorg-xinit cups ipp-usb \
     ghostscript python-psutil fuse dunst wine-staging alacritty brave-bin \
     dmenu gtk-engine-murrine materia-gtk-theme ttf-ubuntu-font-family \
     lib32-gnutls lib32-libldap lib32-libgpg-error lib32-sqlite lib32-libpulse \
-    lib32-alsa-plugins picom lxappearance conky pipewire pavucontrol pulsemixer \
-    pcmanfm gvfs xarchiver rofi galculator geany lxsession curl wget cargo sxiv \
+    lib32-alsa-plugins picom lxappearance conky pipewire pulsemixer \
+    pcmanfm gvfs xarchiver rofi galculator geany lxsession curl wget sxiv \
     scrot feh alsa-utils neofetch htop powerline mpv gamemode \
     polybar bspwm sxhkd playerctl -y
