0a8f8212ead95251755dfb2be3cbe5b85d565812
Colby <colby.braden@gmail.com>
2024-01-11 18:43:57 -0600

    updated sway config

--- arch/sway.sh
+++ arch/sway.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 yay -S --needed sway swaylock swayidle swaybg brave-bin autotiling i3status \
     foot rofi seatd playerctl arc-gtk-theme papirus-icon-theme \
-    lxappearance pipewire pipewire-pulse wireplumber wl-clipboard man-db neofetch \
+    lxappearance nwg-look-bin pipewire pipewire-pulse wireplumber wl-clipboard man-db neofetch \
     gtk-engine-murrine mpd mpv xorg-xwayland gvfs file-roller lxsession \
     gamemode flatpak polkit cups ipp-usb ghostscript dunst imv libadwaita \
     gnome-themes-extra otf-font-awesome ttf-croscore ttf-dejavu ttf-droid \
