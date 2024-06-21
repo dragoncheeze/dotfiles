faaf11fb233ac8bef07a1a693bfe982eae14a64f
Colby <colby.braden@gmail.com>
2024-06-20 22:13:51 -0500

    updated arch default install script

--- arch/defaults.sh
+++ arch/defaults.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 # Default programs
-yay -S rsync man pacman-contrib reflector xclip xorg-xinit cups ipp-usb \
-    ghostscript python-psutil fuse dunst wine-staging alacritty brave-bin \
+sudo pacman -S  --needed rsync man pacman-contrib reflector xclip xorg-xinit cups \
+    ghostscript python-psutil fuse dunst wine-staging alacritty ipp-usb \
     dmenu gtk-engine-murrine materia-gtk-theme ttf-ubuntu-font-family \
     lib32-gnutls lib32-libldap lib32-libgpg-error lib32-sqlite lib32-libpulse \
     lib32-alsa-plugins picom lxappearance conky pipewire pulsemixer \
     pcmanfm gvfs xarchiver rofi galculator geany lxsession curl wget sxiv \
-    scrot feh alsa-utils neofetch htop powerline mpv gamemode \
+    scrot feh alsa-utils fastfetch htop powerline mpv gamemode \
     polybar bspwm sxhkd playerctl -y
