6bc4a694450299542449066aeddf9207d6ee175c
Colby <colby.braden@gmail.com>
2024-06-24 07:44:50 -0500

    updated arch sway install script

--- arch/sway.sh
+++ arch/sway.sh
@@ -6,7 +6,8 @@ sudo pacman -S --needed sway swaylock swayidle swaybg firefox i3status \
     gamemode flatpak polkit cups ipp-usb ghostscript dunst imv libadwaita \
     gnome-themes-extra otf-font-awesome ttf-croscore ttf-dejavu ttf-droid \
     thunar thunar-archive-plugin thunar-volman tumbler fuse geany galculator \
-    gnu-free-fonts ttf-liberation noto-fonts ttf-roboto ttf-ubuntu-font-family &&
+    gnu-free-fonts ttf-liberation noto-fonts ttf-roboto ttf-ubuntu-font-family \
+    materia-gtk-theme nwg-look &&
 # Cups
 sudo systemctl enable cups &&
 sudo systemctl start cups
