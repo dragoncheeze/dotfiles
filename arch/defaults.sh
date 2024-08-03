46ea8c2e2bcc00adf01cf9819acfe68e8641ac8c
Colby <colby.braden@gmail.com>
2024-08-02 21:49:08 -0500

    updated install scripts

--- arch/defaults.sh
+++ arch/defaults.sh
@@ -2,7 +2,7 @@
 # Default programs
 sudo pacman -S  --needed rsync man pacman-contrib reflector xclip xorg-xinit cups \
     ghostscript python-psutil fuse dunst wine-staging alacritty ipp-usb \
-    dmenu gtk-engine-murrine materia-gtk-theme ttf-ubuntu-font-family \
+    dmenu gtk-engine-murrine materia-gtk-theme noto-fonts-emoji ttf-ubuntu-font-family \
     lib32-gnutls lib32-libldap lib32-libgpg-error lib32-sqlite lib32-libpulse \
     lib32-alsa-plugins picom lxappearance conky pipewire pulsemixer \
     pcmanfm gvfs xarchiver rofi galculator geany lxsession curl wget sxiv \
--- arch/sway.sh
+++ arch/sway.sh
@@ -4,7 +4,7 @@ sudo pacman -S --needed sway swaylock swayidle swaybg firefox i3status \
     lxappearance pipewire pipewire-pulse wireplumber wl-clipboard man-db fastfetch \
     gtk-engine-murrine mpd mpv xorg-xwayland gvfs file-roller lxsession \
     gamemode flatpak polkit cups ipp-usb ghostscript dunst imv libadwaita \
-    gnome-themes-extra otf-font-awesome ttf-croscore ttf-dejavu ttf-droid \
+    gnome-themes-extra otf-font-awesome noto-fonts-emoji ttf-croscore ttf-dejavu ttf-droid \
     thunar thunar-archive-plugin thunar-volman tumbler fuse geany galculator \
     gnu-free-fonts ttf-liberation noto-fonts ttf-roboto ttf-ubuntu-font-family \
     materia-gtk-theme nwg-look &&
