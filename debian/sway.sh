9d824b8dc749646c76b85da725b6ba3885f58d24
Colby <colby.braden@gmail.com>
2024-08-27 09:15:14 -0500

    updated sway config

--- debian/sway.sh
+++ debian/sway.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 sudo apt install sway swaybg swaylock swayidle \
     python3-i3ipc seatd dunst light playerctl \
-    i3status pipewire-audio \
+    i3status pipewire-audio nwg-look \
     wl-clipboard gtk2-engines-murrine mpv mpd foot \
     papirus-icon-theme arc-theme rofi fonts-dejavu fonts-font-awesome \
     fonts-liberation2 fonts-noto fonts-roboto lxpolkit libglib2.0-bin xwayland \
