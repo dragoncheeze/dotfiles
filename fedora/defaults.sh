1f03f2c7ff5535a819f59fbcb8a2299117207727
Colby Braden <colby@fedora.lan>
2024-09-28 20:57:10 -0500

    updated fedora default install script

--- fedora/defaults.sh
+++ fedora/defaults.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-sudo dnf install sway thunar thunar-volman thunar-archive-plugin tumbler tumbler-extras \
+sudo dnf install tar sway thunar thunar-volman thunar-archive-plugin tumbler tumbler-extras \
     lxpolkit i3status python3-i3ipc firefox mpv cups avahi galculator gamemode imv \
     clipman playerctl papirus-icon-theme papirus-icon-theme-dark rofi dunst fastfetch \
     fuse geany evince simple-scan usbutils @multimedia @sound-and-video lxappearance \
@@ -11,10 +11,10 @@ git clone https://github.com/nwg-piotr/autotiling ~/builds/autotiling &&
 # Flatpak Repo
 flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo &&
 # Brave
-sudo dnf install dnf-plugins-core &&
-    sudo dnf config-manager --add-repo https://brave-browser-rpm-release.s3.brave.com/brave-browser.repo &&
-    sudo rpm --import https://brave-browser-rpm-release.s3.brave.com/brave-core.asc &&
-    sudo dnf install brave-browser &&
+#sudo dnf install dnf-plugins-core &&
+#    sudo dnf config-manager --add-repo https://brave-browser-rpm-release.s3.brave.com/brave-browser.repo &&
+#    sudo rpm --import https://brave-browser-rpm-release.s3.brave.com/brave-core.asc &&
+#    sudo dnf install brave-browser &&
 # RPM Fusion
 sudo dnf install https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm &&
     sudo dnf config-manager --enable fedora-cisco-openh264 &&
