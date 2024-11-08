6d018d65285de0f5745238db5f59d25ab6d6a31c
Colby <colby.braden@gmail.com>
2024-11-08 09:32:54 -0600

    added wine and lutris to fedora script

--- fedora/defaults.sh
+++ fedora/defaults.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-sudo dnf install tar sway thunar thunar-volman thunar-archive-plugin tumbler tumbler-extras \
+sudo dnf install tar sway wine lutris thunar thunar-volman thunar-archive-plugin tumbler tumbler-extras \
     lxpolkit i3status python3-i3ipc firefox mpv cups avahi galculator gamemode imv \
     clipman playerctl papirus-icon-theme papirus-icon-theme-dark rofi dunst fastfetch \
     fuse geany evince simple-scan usbutils @multimedia @sound-and-video lxappearance \
