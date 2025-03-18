8670ba61117e0993db92d347c8d746b563f1ff1e
Colby <colby.braden@gmail.com>
2025-03-18 07:56:24 -0500

    updated some more configs

--- arch/sway.sh
+++ arch/sway.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 sudo pacman -S --needed vim pacman-contrib sway swaylock swayidle swaybg i3status \
-    foot rofi seatd playerctl papirus-icon-theme rsync uwsm\
-    lxappearance pipewire pipewire-pulse wireplumber wl-clipboard man-db fastfetch \
-    gtk-engine-murrine mpd mpv xorg-xwayland gvfs xarchiver evince simple-scan lxsession \
+    foot rofi seatd playerctl papirus-icon-theme rsync uwsm fastfetch \
+    lxappearance pipewire pipewire-pulse wireplumber wl-clipboard man-db \
+    gtk-engine-murrine mpd mpv xorg-xwayland gvfs xarchiver evince simple-scan \
     gamemode flatpak polkit cups ipp-usb ghostscript dunst imv libadwaita \
     gnome-themes-extra otf-font-awesome noto-fonts-emoji ttf-croscore ttf-dejavu ttf-droid \
     thunar thunar-archive-plugin thunar-volman tumbler fuse geany galculator \
     gnu-free-fonts ttf-liberation noto-fonts ttf-roboto ttf-ubuntu-font-family \
     nwg-look autotiling materia-gtk-theme nwg-look noto-fonts-emoji fwupd \
-    xdg-desktop-portal-gtk xdg-desktop-portal-wlr &&
+    xdg-desktop-portal-gtk xdg-desktop-portal-wlr lxsession &&
 # Cups
 sudo systemctl enable cups &&
 sudo systemctl start cups
--- config/fish/config.fish
+++ config/fish/config.fish
@@ -1,5 +1,5 @@
 alias v="nvim"
-#alias sway="sway --unsupported-gpu -D noscanout"
+#alias sway="sway --unsupported-gpu"
 alias reboot="systemctl reboot"
 alias shutdown="systemctl poweroff"
 #alias backports="sudo apt -t bookworm-backports install"
