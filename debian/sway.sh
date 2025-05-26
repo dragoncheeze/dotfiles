5fe34cffc98f4794b4dc0f005093406d98975c6d
Colby <colby.braden@gmail.com>
2025-05-26 15:37:44 -0500

    updated sway install script

--- debian/sway.sh
+++ debian/sway.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 sudo apt install sway swaybg swaylock swayidle \
-    python3-i3ipc seatd dunst light playerctl \
-    i3status pipewire-audio nwg-look fwupd\
-    wl-clipboard gtk2-engines-murrine mpv mpd foot \
+    python3-i3ipc seatd dunst light playerctl autotiling \
+    i3status pipewire-audio nwg-look fwupd fastfetch \
+    wl-clipboard gtk2-engines-murrine mpv mpd foot xdg-desktop-portal-wlr \
     papirus-icon-theme arc-theme rofi fonts-dejavu fonts-font-awesome \
     fonts-liberation2 fonts-noto fonts-roboto lxpolkit libglib2.0-bin xwayland \
     file-roller pcmanfm cups galculator build-essential gamemode imv \
