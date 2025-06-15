d9a5c85b3ea43408b2fd6ab913a1dfbf73189931
Colby <colby.braden@gmail.com>
2025-06-15 12:08:07 -0500

    updated debian configs for trixie.

--- debian/defaults.sh
+++ debian/defaults.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 ## Default programs
 sudo apt install xserver-xorg xorg lxpolkit rsync gtk2-engines-murrine \
-    fonts-ubuntu fonts-dejavu fonts-font-awesome fonts-liberation2 \
+    fonts-ubuntu autotiling fonts-dejavu fonts-font-awesome fonts-liberation2 \
     fonts-recommended fonts-noto fonts-roboto libxkbcommon-dev lxappearance \
-    pavucontrol pulsemixer materia-gtk-theme arc-theme gnome-themes-extra \
-    papirus-icon-theme pcmanfm xarchiver rofi libxft-dev libx11-dev libxinerama-dev \
+    pavucontrol pulsemixer gnome-themes-extra papirus-icon-theme \
+    pcmanfm xarchiver rofi libxft-dev libx11-dev libxinerama-dev \
     galculator geany dunst libnotify-bin suckless-tools build-essential curl \
     wget sxiv scrot feh htop mpv synaptic gamemode wine picom fwupd libglib2.0-bin \
-    i3status python3-i3ipc playerctl cups pipewire-audio libncurses-dev -y
+    i3 i3status python3-i3ipc playerctl cups pipewire-audio libncurses-dev -y
 
