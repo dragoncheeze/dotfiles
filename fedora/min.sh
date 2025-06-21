6b7e8a79610ea91a35eddfbf181c6041f358554d
Colby <colby.braden@gmail.com>
2025-06-21 10:57:26 -0500

    updated fedora scripts

---
+++ fedora/min.sh
@@ -0,0 +1,2 @@
+sudo dnf install @multimedia @sound-and-video system-config-printer cups cups-filters flatpak &&	
+flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
