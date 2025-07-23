4b50142b1907c66f672a58191cb9bb7d34d01101
Colby <colby.braden@gmail.com>
2025-07-23 08:57:34 -0500

    added icon folder and removed old install scripts

--- debian/fastfetch.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-sudo apt install cmake pkg-config -y &&
-git clone https://github.com/fastfetch-cli/fastfetch.git  ~/builds/fastfetch/ &&
-cd ~/builds/fastfetch &&
-mkdir -p build && cd build &&
-cmake .. &&
-cmake --build . --target fastfetch --target flashfetch &&
-cp fastfetch ~/bin/ &&
-clear && echo fastfetch installed.
